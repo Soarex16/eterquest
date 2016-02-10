de90b30046ab07839398ad3431d405e3947437a2
B10nicMachine <shumik1337@gmail.com>
2016-02-10 15:03:19 +0300

    icon loading

--- src/main/java/com/soarex/eterquest/Eterquest.java
+++ src/main/java/com/soarex/eterquest/Eterquest.java
@@ -1,11 +1,47 @@
 package com.soarex.eterquest;
 
+import com.soarex.eterquest.util.ImageUtil;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 /**
  * Created by shumaf on 10.02.16.
  */
 public class Eterquest {
 
-    public static void main(String[] args) {
+    private static int WIDTH = 1280;
+    private static int HEIGHT = 720;
+    private static int FPS = 60;
 
+    public static void main(String[] args) {
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.setTitle("Eterquest");
+            Display.setIcon(new ByteBuffer[]{
+                ImageUtil.loadIcon(Eterquest.class.getResourceAsStream("/icon/icon16.png")),
+                ImageUtil.loadIcon(Eterquest.class.getResourceAsStream("/icon/icon32.png")),
+                ImageUtil.loadIcon(Eterquest.class.getResourceAsStream("/icon/icon128.png")),
+            });
+            Display.create(new PixelFormat().withDepthBits(24));
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        GL11.glViewport(0, 0, WIDTH, HEIGHT);
+
+        while (!Display.isCloseRequested()) {
+            Display.sync(FPS);
+            Display.update();
+        }
+
+        Display.destroy();
     }
 }
---
+++ src/main/java/com/soarex/eterquest/util/ImageUtil.java
@@ -0,0 +1,26 @@
+package com.soarex.eterquest.util;
+
+import org.newdawn.slick.opengl.PNGDecoder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by shumaf on 10.02.16.
+ */
+public class ImageUtil {
+
+    public static ByteBuffer loadIcon(InputStream is) throws IOException {
+        try {
+            PNGDecoder decoder = new PNGDecoder(is);
+            ByteBuffer bb = ByteBuffer.allocateDirect(decoder.getWidth()*decoder.getHeight()*4);
+            decoder.decode(bb, decoder.getWidth()*4, PNGDecoder.RGBA);
+            bb.flip();
+            return bb;
+        } finally {
+            is.close();
+        }
+    }
+}

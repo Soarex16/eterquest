75a3d793aff1b780097136ddb1a40104b149853a
B10nicMachine <shumik1337@gmail.com>
2016-02-14 19:02:35 +0300

    =/

--- src/main/java/com/soarex/eterquest/engine/Game.java
+++ src/main/java/com/soarex/eterquest/engine/Game.java
@@ -11,8 +11,10 @@ import com.soarex.eterquest.engine.util.MiscUtil;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.PixelFormat;
 
 import static org.lwjgl.opengl.GL11.*;
 
